39b59876c10ac028e5dcd446e24fbc4ec5e603f4
YuhengHuang42 <yuheng.huang42@gmail.com>
2022-03-29 08:03:20 +0900

    Add example

--- train_vit.sh
+++ train_vit.sh
@@ -1,14 +1,14 @@
-#python -u train.py --exp-name train_vit_mask_width3 \
-#        --n-gpu 1 --model-arch ViT_b16_224 --dataset ImageNet \
-#        --num-classes 1000 --image-size 224 --mask-width 3 \
-#        --data-dir /MALEI/ --output-dir ./output/patchveto \
+python -u train.py --exp-name train_vit_mask_width3 \
+        --n-gpu 1 --model-arch ViT_b16_224 --dataset ImageNet \
+        --num-classes 1000 --image-size 224 --mask-width 3 \
+        --data-dir /MALEI/ --output-dir ./output/patchveto \
 
-#python -u test_verified.py --exp-name vit_imagenet_mask3_verify \
-#--model-arch ViT_b16_224 --dataset partImageNet --num-classes 1000 \
-#--patch-size 16  --mask-size 48 --checkpoint-path /home/malei/yuheng_workspace/voterStudy/output/patchveto/save/train_vit_mask32/checkpoints/best.pth \
-#--data-dir "/MALEI/partImageNet/" --output-dir ./output/patchveto
+python -u test_verified.py --exp-name vit_imagenet_mask3_verify \
+--model-arch ViT_b16_224 --dataset partImageNet --num-classes 1000 \
+--patch-size 16  --mask-size 48 --checkpoint-path /home/malei/yuheng_workspace/voterStudy/output/patchveto/save/train_vit_mask32/checkpoints/best.pth \
+--data-dir "/MALEI/partImageNet/" --output-dir ./output/patchveto
 
 python -u test_verified.py --exp-name vit_imagenet_val_mask3_verify \
 --model-arch ViT_b16_224 --dataset ImageNet --num-classes 1000 \
 --patch-size 16  --mask-size 48 --checkpoint-path /home/malei/yuheng_workspace/voterStudy/output/patchveto/save/train_vit_mask_width3/checkpoints/best.pth \
---data-dir "/MALEI/" --output-dir ./output/patchveto 
+--data-dir "/MALEI/" --output-dir ./output/patchveto --skip 10 --result result_vit_imagenet_val.pt
